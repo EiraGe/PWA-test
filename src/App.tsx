02d80d198ee4b127de480ff718ac53971cc97e15
Ella Ge <eirage@chromium.org>
2023-07-11 12:55:41 -0400

    Basic layout and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Button from '@mui/material/Button';
+import { Box, Grid, InputLabel, TextField, Paper } from "@mui/material";
 
-import './App.css';
+import "./App.css";
 
 type Manifest = {
   name: string;
@@ -12,12 +12,12 @@ type Manifest = {
   display: string;
   scope: string;
   start_url: string;
-}
+};
 
-function readLSManifest() : Manifest | null {
+function readLSManifest(): Manifest | null {
   let manifest = localStorage.getItem("manifest");
   if (!manifest) {
-    return null
+    return null;
   }
   return JSON.parse(manifest);
 }
@@ -26,21 +26,21 @@ function updateToLS(manifest: Manifest) {
   localStorage.setItem("manifest", JSON.stringify(manifest));
 }
 
-function initManifest(scope: string) : Manifest {
+function initManifest(scope: string): Manifest {
   return {
     name: "PWA-test",
     description: "A test PWA",
     icons: [
       {
-        "src": scope + "/logo192.png",
-        "type": "image/png",
-        "sizes": "192x192"
+        src: scope + "/logo192.png",
+        type: "image/png",
+        sizes: "192x192",
       },
       {
-        "src": scope + "/logo512.png",
-        "type": "image/png",
-        "sizes": "512x512"
-      }
+        src: scope + "/logo512.png",
+        type: "image/png",
+        sizes: "512x512",
+      },
     ],
     background_color: "white",
     theme_color: "white",
@@ -64,37 +64,93 @@ function setPageManifest(manifest: Manifest) {
   e.setAttribute("href", manifestURL);
 }
 
-function prepareManifest() : Manifest {
+function prepareManifest(): Manifest {
   let manifest = readLSManifest();
   if (!manifest) {
-    manifest = initManifest(window.location.origin)
+    manifest = initManifest(window.location.origin);
   }
-  return manifest
+  return manifest;
 }
 
-function updateDisplayedForm(manifest: Manifest) {
-
-}
+function updateDisplayedForm(manifest: Manifest) {}
 
 function getManifestFromForm(): Manifest {
   return initManifest(window.location.origin);
 }
 
 function onFormChanged() {
-  let newManifest = getManifestFromForm()
-  updateToLS(newManifest)
-  setPageManifest(newManifest)
+  let newManifest = getManifestFromForm();
+  updateToLS(newManifest);
+  setPageManifest(newManifest);
+}
+
+function ManifestItemLabel(props: any) {
+  return (
+    <InputLabel
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        fontWeight: 700,
+      }}
+    >
+      {props.name}
+    </InputLabel>
+  );
+}
+function ManifestItem(props: any) {
+  return (
+    <TextField
+      id={props.id}
+      type={props.type || "text"}
+      label={props.name}
+      fullWidth
+      size="small"
+      autoComplete="off"
+      variant="outlined"
+    />
+  );
 }
 
 function App() {
-  let manifest = prepareManifest()
-  setPageManifest(manifest)
-  updateDisplayedForm(manifest)
+  let manifest = prepareManifest();
+  setPageManifest(manifest);
+  updateDisplayedForm(manifest);
 
   return (
-    <div>
-      <Button variant="contained">Hello World</Button>
-    </div>
+    <Paper elevation={3} sx={{ marginRight: "15%", marginLeft: "15%" }}>
+      <Box sx={{ padding: 5 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={4} sm={2}>
+            <ManifestItemLabel name="Name" />
+          </Grid>
+          <Grid item xs={6} sm={4}>
+            <ManifestItem id="manifest-name" label="Name" />
+          </Grid>
+          <Grid item xs={4} sm={2}>
+            <ManifestItemLabel name="Short Name" />
+          </Grid>
+          <Grid item xs={6} sm={4}>
+            <ManifestItem id="manifest-short-name" label="ShortName" />
+          </Grid>
+          <Grid item xs={4} sm={2}>
+            <ManifestItemLabel name="Start URL" />
+          </Grid>
+          <Grid item xs={8} sm={9}>
+            <ManifestItem
+              id="manifest-start-url"
+              label="Start URL"
+              type="url"
+            />
+          </Grid>
+          <Grid item xs={4} sm={2}>
+            <ManifestItemLabel name="Scope" />
+          </Grid>
+          <Grid item xs={8} sm={9}>
+            <ManifestItem id="manifest-scope" label="Scope" type="url" />
+          </Grid>
+        </Grid>
+      </Box>
+    </Paper>
   );
 }
 
