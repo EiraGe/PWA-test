b6cfcaf537ea8bdbc3a812708708ca675841af7a
Ella Ge <eirage@chromium.org>
2025-03-13 16:10:55 +0000

    Add install button

--- src/Manifest.tsx
+++ src/Manifest.tsx
@@ -1,5 +1,6 @@
-import * as React from "react";
+import React from "react";
 import {
+  Button,
   Collapse,
   Divider,
   FormControlLabel,
@@ -12,9 +13,39 @@ import { ManifestType } from "./ManifestType";
 import { generateManifestText, setManifestLink } from "./ManifestMediator";
 import { updateToLS } from "./ManifestLocalStorageMediator";
 
+export interface IBeforeInstallPromptEvent extends Event {
+  readonly platforms: string[];
+  readonly userChoice: Promise<{
+    outcome: 'accepted' | 'dismissed';
+    platform: string;
+  }>;
+  prompt(): Promise<void>;
+}
+export type PromptCtx = {
+  deferredEvt: IBeforeInstallPromptEvent | null;
+  hidePrompt?: () => void;
+};
+export type Children = {
+  children: React.ReactElement | React.ReactElement[] | string | null;
+};
+
 export function Manifest() {
   const [hasManifest, setHasManifest] = React.useState(true);
   const [stringManifest, setStringManifest] = React.useState<string>("");
+  const [installPrompt, setInstallPrompt] = React.useState<any>(null);
+
+  React.useEffect(() => {
+    const ready = (e: IBeforeInstallPromptEvent) => {
+      console.info("got beforeinstallprompt", e);
+      setInstallPrompt(e)
+    }
+
+    window.addEventListener('beforeinstallprompt', ready as any);
+
+    return () => {
+      window.removeEventListener('beforeinstallprompt', ready as any);
+    }
+  }, []);
 
   const handleShowManifestChange = () => {
     setHasManifest((prev) => !prev);
@@ -49,6 +80,12 @@ export function Manifest() {
         }
         label="Add Manifest"
       />
+      {
+        installPrompt !== null ?
+          <Button>
+            Install
+          </Button> : null
+      }
       <Collapse in={hasManifest}>
         <ManifestControl
           setManifestHandler={handleSetManifest}></ManifestControl>
